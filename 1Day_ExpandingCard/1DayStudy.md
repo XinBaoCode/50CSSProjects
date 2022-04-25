4c9abec4cdd38b1b7cff5c24530c23b812619921
XinBaoCode <1392368311@qq.com>
2022-04-25 21:18:28 +0800

    1day need css3 md

---
+++ 1Day_ExpandingCard/1DayStudy.md
@@ -0,0 +1,15 @@
+# 学习到的技术
+## CSS
+### Flex
+我们发现在这个项目中，很多时候使用到了flex:1,flex:10的概念，所以有必要在这对flex技术进行解释。
+**flex后接一个数字，这个数字的值代表flex-grow的值**
+<img src="../image/1_Flex.png" />
+这里解释一下flex-grow,flex-shrink和flex-basis的含义
+|  名称 | 含义  |
+| ---- | ---- |
+| flex-grow |  flex-grow 定义项目的放大比例，默认为0，即如果存在剩余空间，也不放大 |
+| flex-shrink |flex-shrink 定义了项目的缩小比例，默认为1，即如果空间不足，该项目将缩小 |
+| flex-basis | flex-basis 给上面两个属性分配多余空间之前, 计算项目是否有多余空间, 默认值为 auto, 即项目本身的大小 |
+这里的flex-grow为10的意思就是选中的图片是其他的未选中图片大小的10倍。
+
+### CSS3的过渡动画
--- 1Day_ExpandingCard/script.js
+++ 1Day_ExpandingCard/script.js
@@ -1,9 +1,33 @@
+const bgText = [
+  {
+    text: 'Expore The World',
+    imgUrl: 'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  },
+  {
+    text: 'Wild Forest',
+    imgUrl: 'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  },
+  {
+    text: 'City on Winter',
+    imgUrl: 'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+  },
+  {
+    text: 'Mountains - Clouds',
+    imgUrl: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  },
+]
+
 const panels = document.querySelectorAll('.panel')
 
-panels.forEach((panel) => {
+panels.forEach((panel, index) => {
+  // 循环加入文本和Txt
+  panel.style.backgroundImage = `url(${bgText[index].imgUrl})`
+  const txt = document.createTextNode(bgText[index].text)
+  panel.firstElementChild.appendChild(txt)
+
+  // 监听点击事件
   panel.addEventListener('click', () => {
     removeActiveClasses()
-    console.log('监听ing')
     panel.classList.add('active')
   })
 })
